eed57ef5ebf84973a4ecc34e5ce612879b7d0fa7
Alberto-R-L <albertorl003@gmail.com>
2020-11-10 14:37:40 +0100

    a

---
+++ Worksheet1-1.js
@@ -0,0 +1,7 @@
+
+
+var nEnlaces = document.getElementsByTagName("a").length;
+document.write("Numero de enlaces: " + nEnlaces + "<br>");
+
+var penu = document.getElementById("a").;
+document.write(penu)
